dc0eebf9a4cacdb78a481dc00f8950f122aaa025
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-25 14:01:58 -0400

    Steps 3 and 4

--- cc_5.js
+++ cc_5.js
@@ -1,4 +1,4 @@
-// Step 2 Employee Array
+// Step 2: Employee Array
 
 const employees = [
   { name: "Alice", hourlyRate: 25, hoursWorked: 38 },
@@ -6,4 +6,17 @@ const employees = [
   ( name: "Joe", hourlyRate: 22, hoursWorked: 31)
 ];
 
-// Step 3
+// Step 3: Pay for up to 40 hours
+
+function calculateBasePay(rate, hours) {
+    return rate * Math.min(hours, 40);
+}
+
+// Step 4: Overtime 1.5x over 40 hours
+
+function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+        return (hours - 40) * (rate * 1.5);
+    }
+    return 0;
+}
