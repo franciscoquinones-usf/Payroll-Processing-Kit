74b8fca850d9b32e5ad4080d0690f73a4fa8f276
Francisco Quinones <fquinonescedeno@usf.edu>
2025-09-25 13:55:34 -0400

    first commit

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+// Step 2 Employee Array
+
+const employees = [
+  { name: "Alice", hourlyRate: 25, hoursWorked: 38 },
+  { name: "Bob", hourlyRate: 20, hoursWorked: 45 },
+  ( name: "Joe", hourlyRate: 22, hoursWorked: 31)
+];
+
+// Step 3
